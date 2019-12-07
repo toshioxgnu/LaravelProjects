482eadcd91262580a92d1773167f7c8fd40f4bd1
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-12-07 10:06:23 -0300

    empezando

---
+++ todos-app/resources/views/todos/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Todos</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>
+		Todos Page 
+	</h1>
+</body>
+</html>
