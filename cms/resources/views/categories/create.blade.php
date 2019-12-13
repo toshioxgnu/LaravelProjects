c19888a862fbeb67a3b30b8fd23b7c268e19df20
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2019-12-12 21:41:06 -0300

    Empezando segundo proyecto

---
+++ cms/resources/views/categories/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+	<div class="card card-default">
+		<div class="card-header">Create</div>
+		<div class="card-body">
+			<form action="">
+				<div class="form-group">
+					<label for="name">Name</label>
+					<input type="text" id="name" class="form-control" name="name">
+				</div>
+					<a href="" class="btn btn-success" title="Create"><i class="fas fa-plus-square"></i></a>
+
+			</form>
+		</div>
+	</div>
+@endsection 
